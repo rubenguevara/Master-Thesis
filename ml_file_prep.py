a19ecb83c94f9d61f3cb390d69cfe6a1232f17ca
rubenguevara <ruguevara98@gmail.com>
2022-10-19 15:31:06 +0200

    updated code

--- ml_file_prep.py
+++ ml_file_prep.py
@@ -1,8 +1,10 @@
 import pandas as pd
-import os
+import os, time
 from EventIDs import IDs
 import numpy as np
 import uproot as up
+
+t0 = time.time()
 
 save_dir = "../../../storage/racarcam/"
 dm1 = save_dir + "DM1.root"
@@ -10,7 +12,6 @@ Run2_bkgs = save_dir + "Run2.root"
 
 
 thing = up.open(Run2_bkgs)
-print(thing.keys())
 
 tree_a = thing['id_mc16a']
 tree_d = thing['id_mc16d']
@@ -27,15 +28,63 @@ dfs = [df1, df2, df3]
 df = pd.concat(dfs).sample(frac=1, random_state=42).reset_index(drop=True)  # Shuffle and fix indices
 print(df)
 
-df['Label'] = np.isin(df['RunNumber'], IDs["all_bkg"]).astype(int)
+df['Label'] = np.isin(df['RunNumber'], IDs["dm_sig"]).astype(int)
 print(df)
 
+t = "{:.2f}".format(int( time.time()-t0 )/60.)
+print( "---"*40)
+print( "Time spent making bkg df: "+str(t)+" min")  
+print( "---"*40)
 
-# # save_dir = "ML_Files"
-# # try:
-# #     os.makedirs(save_dir)
+t1 = time.time()
 
-# # except FileExistsError:
-# #     pass
+dm_thing = up.open(dm1)
 
-# # df.to_csv(save_dir+'/Run2Bkgs.csv')
+dm_tree_a = dm_thing['id_mc16a']
+dm_tree_d = dm_thing['id_mc16d']
+dm_tree_e = dm_thing['id_mc16e']
+
+dm_df1 = dm_tree_a.arrays(library="pd")
+dm_df2 = dm_tree_d.arrays(library="pd")
+dm_df3 = dm_tree_e.arrays(library="pd")
+print(dm_df1)
+print(dm_df2)
+print(dm_df3)
+
+dm_dfs = [dm_df1, dm_df2, dm_df3]
+dm_df = pd.concat(dm_dfs).sample(frac=1, random_state=42).reset_index(drop=True)  # Shuffle and fix indices
+print(dm_df)
+
+dm_df['Label'] = np.isin(dm_df['RunNumber'], IDs["dm_sig"]).astype(int)
+print(dm_df)
+
+t = "{:.2f}".format(int( time.time()-t1 )/60.)
+print( "---"*40)
+print( "Time spent making sig df: "+str(t)+" min")  
+print( "---"*40)
+
+
+t2 = time.time()
+
+s_and_b = [df, dm_df]
+df_tot = pd.concat(s_and_b).sample(frac=1, random_state=42).reset_index(drop=True)  # Shuffle and fix indices
+print(df_tot)
+
+t = "{:.2f}".format(int( time.time()-t2 )/60.)
+print( "---"*40)
+print( "Time spent making mixed df: "+str(t)+" min")  
+print( "---"*40)
+
+t = "{:.2f}".format(int( time.time()-t0 )/60.)
+print( "---"*40)
+print( "TOTAL time spent preparing df: "+str(t)+" min")  
+print( "---"*40)
+
+t3 = time.time()
+# df_tot.to_hdf(save_dir+'DM1_Run2.hdf5', 'DM1_Run2', mode = 'w')
+df_tot.to_csv(save_dir+'DM1_Run2.csv')
+
+t = "{:.2f}".format(int( time.time()-t3 )/60.)
+print( "---"*40)
+print( "Time spent saving file: "+str(t)+" min")  
+print( "---"*40)
