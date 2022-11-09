4eae51348e8575e79ef7e94939cf5b102d2bd797
rubenguevara <ruguevara98@gmail.com>
2022-11-09 14:13:16 +0100

    Optimized code

--- ML/xgb.py
+++ ML/xgb.py
@@ -1,63 +1,70 @@
 import xgboost as xgb
 import pandas as pd
-import numpy as np
 from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import os
 
+print(xgb.__version__)
+
 save_dir = "../../../storage/racarcam/"
-filename = "Stat_red_DM_Run2_50MET.h5"
+filename = 'Full_DM_sig.h5'
 
 df = pd.read_hdf(save_dir+filename, key='df_tot')
 
-X = df.copy()
-df_EventNumber = X.pop('EventID')
-W = np.abs(np.array(X.pop('Weight')*0.2))
-df_CrossSection = X.pop('CrossSection')
-df_RunNumber = X.pop('RunNumber')
-df_RunPeriod = X.pop('RunPeriod')
+df_features = df.copy()
+df_EventID = df_features.pop('EventID')
+df_CrossSection = df_features.pop('CrossSection')
+df_RunNumber = df_features.pop('RunNumber')
+df_RunPeriod = df_features.pop('RunPeriod')
+df_dPhiCloseMet = df_features.pop('dPhiCloseMet')                             # Bad variable
+df_dPhiLeps = df_features.pop('dPhiLeps')                                     # Bad variable
 
-Y = np.array(X.pop('Label'))
-features = np.array(X.columns)
-X = np.array(X)
-size = int(len(Y)*0.2)
+df_labels = df_features.pop('Label')
 
-X_train = np.concatenate((X[Y==0][0:size],X[Y==1][0:size]), axis=0)
-w_train = np.concatenate((W[Y==0][0:size],W[Y==1][0:size]), axis=0)
-y_train = np.concatenate((Y[Y==0][0:size],Y[Y==1][0:size]), axis=0)
-X_valid = np.concatenate((X[Y==0][size:size*2],X[Y==1][size:size*2]), axis=0)
-y_valid = np.concatenate((Y[Y==0][size:size*2],Y[Y==1][size:size*2]), axis=0)
-X_eval = np.concatenate((X[Y==0][size*2:size*3],X[Y==1][size*2:size*3]), axis=0)
-y_eval = np.concatenate((Y[Y==0][size*2:size*3],Y[Y==1][size*2:size*3]), axis=0)
-# Get the sums of the weights for signal and background events 
-sum_wsig = sum( w_train[i] for i in range(len(y_train)) if y_train[i] == 1.0  )
-sum_wbkg = sum( w_train[i] for i in range(len(y_train)) if y_train[i] == 0.0  )
+X_train, X_test, Y_train, Y_test = train_test_split(df_features, df_labels, test_size=0.2, random_state=42)
+W_train = abs(X_train.pop('Weight'))
+W_test = X_test.pop('Weight')
 
+sum_wsig = sum(W_train[Y_train==1])
+sum_wbkg = sum(W_train[Y_train==0])
 
 xgbclassifier = xgb.XGBClassifier(
     max_depth=3, 
     use_label_encoder=False,
     n_estimators=120,
     learning_rate=0.1,
-    #n_jobs=4,
+    predictor = 'cpu_predictor',
+    tree_method = 'hist',
     scale_pos_weight=sum_wbkg/sum_wsig,
     objective='binary:logistic',
-    eval_metric='error',
-    # eval_metric='logloss',
+    # eval_metric='error@0.7',
+    eval_metric='auc',
     missing=10,
-    random_state=42) 
+    random_state=42,
+    verbosity = 1) 
 
-xgbclassifier.fit(X_train, y_train, sample_weight=w_train) 
+print('Starting fitting')
+xgbclassifier.fit(X_train, Y_train, sample_weight = W_train, verbose = True)#, eval_set = (X_test, Y_test) ) 
 
-# Test the BDT performance using the validation dataset
-y_pred = xgbclassifier.predict( X_valid ) # The actual signal/background predictions. Note that we don't actually use them
-y_pred_prob = xgbclassifier.predict_proba( X_valid ) # The BDT outputs for each event
+model_dir = 'Models/XGB/'
+try:
+    os.makedirs(model_dir)
 
-#  histogram of the BDT outputs
-n, bins, patches = plt.hist(y_pred_prob[:,1][y_valid==0], 200, facecolor='blue', alpha=0.2,label="Background")
-n, bins, patches = plt.hist(y_pred_prob[:,1][y_valid==1], 200, facecolor='red', alpha=0.2, label="Signal")
+except FileExistsError:
+    pass
+xgbclassifier.save_model(model_dir+'FULL.txt')
 
-plot_dir = 'Plots_XGBoost/'
+# To load
+# model_xgb = xgb.Booster()
+# model_xgb.load_model(model_dir+'FULL.txt')
+
+# y_pred = xgbclassifier.predict(X_test)
+y_pred_prob = xgbclassifier.predict_proba(X_test)
+
+### Plotting
+
+plot_dir = 'Plots_XGBoost/FULL/'
 
 try:
     os.makedirs(plot_dir)
@@ -67,33 +74,49 @@ except FileExistsError:
 
 
 plt.figure(1, figsize=[10,6])
+n, bins, patches = plt.hist(y_pred_prob[:,1][Y_test==0], weights = W_test[Y_test==0], bins = 100, facecolor='blue', alpha=0.2,label="Background")
+n, bins, patches = plt.hist(y_pred_prob[:,1][Y_test==1], weights = W_test[Y_test==1], bins = 100, facecolor='red', alpha=0.2, label="Signal")
 plt.xlabel('XGBoost output')
 plt.ylabel('Events')
-plt.title('XGBoost output, DM dataset, validation data')
+plt.yscale('log')
+plt.xlim([0,1])
+plt.title('XGBoost output, FULL DM dataset, weighted validation data')
 plt.grid(True)
 plt.legend()
-plt.savefig(plot_dir+'VAL_red.pdf')
+plt.savefig(plot_dir+'VAL_w.pdf')
 plt.show()
 
-fpr, tpr, thresholds = roc_curve(y_valid,y_pred_prob[:,1], pos_label=1)
+fpr, tpr, thresholds = roc_curve(Y_test, y_pred_prob[:,1], pos_label=1)
 roc_auc = auc(fpr,tpr)
 plt.figure(2)
 lw = 2
 plt.plot(fpr, tpr, color='darkorange',
         lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
 plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
+plt.xlim([-0.01, 1.02])
+plt.ylim([-0.01, 1.02])
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
-plt.title('ROC for xgBoost on DM dataset')
+plt.title('ROC for XGBoost on FULL DM dataset')
 plt.legend(loc="lower right")
-plt.savefig(plot_dir+'ROC_red.pdf')
+plt.savefig(plot_dir+'ROC.pdf')
 plt.show()
 
-sorted_idx = xgbclassifier.feature_importances_.argsort()
+plt.figure(3, figsize=[10,6])
+n, bins, patches = plt.hist(y_pred_prob[:,1][Y_test==0], bins = 100, facecolor='blue', alpha=0.2,label="Background")
+n, bins, patches = plt.hist(y_pred_prob[:,1][Y_test==1], bins = 100, facecolor='red', alpha=0.2, label="Signal")
+plt.xlabel('XGBoost output')
+plt.ylabel('Events')
+plt.yscale('log')
+plt.xlim([0,1])
+plt.title('XGBoost output, FULL DM dataset, unweighted validation data')
+plt.grid(True)
+plt.legend()
+plt.savefig(plot_dir+'VAL_uw.pdf')
+plt.show()
 
-plt.figure(3, figsize=[20,12])
-xgb.plot_importance(xgbclassifier).set_yticklabels(features[sorted_idx][1:])
-plt.savefig(plot_dir+'feature_red.pdf')
+
+fig, ax = plt.subplots(1, 1, figsize = [10, 6])
+xgb.plot_importance(booster = xgbclassifier, ax=ax)
+plt.savefig(plot_dir+'features.pdf')
 plt.show()
